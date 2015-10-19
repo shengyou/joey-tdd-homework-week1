254bb1fa3a6ac6f674c029e7b87af9ef6b02b2b0
Shengyou Fan <shengyoufan@gmail.com>
2015-10-19 17:40:29 +0800

    init: phpunit and project structure

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit_Framework_TestCase;
+
+class CalculatorTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testCase()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+        $this->assertTrue(true);
+    }
+}
